e6df71581f230da0abb1e1bfd329ec361b9d8f05
CSVEconomics <csveconomics@gmail.com>
2025-06-30 17:20:12 +0200

    Style: Global CSS hinzugefügt und Design verbessert

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 // pages/_app.tsx
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
-import '@/styles/globals.css'; // falls du globale Styles nutzt
+import '../styles/globals.css'; // falls du globale Styles nutzt
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
